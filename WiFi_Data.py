19d2719028a56220a8f0a6122426803e32b36810
Dinith Herath <dinithherath18332@gmail.com>
2019-08-29 04:12:30 +0530

    csv file header created and csv file generated.

--- WiFi_Data.py
+++ WiFi_Data.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt  # mqtt client library
 import pandas as pd  # data processing library
 import ast  # library for data validation
 import os
+import csv  # library for handling csv files
 
 broker_url = "broker.hivemq.com"  # broker URL
 broker_port = 1883  # broker port
@@ -23,7 +24,8 @@ def data_store(data_recieved):  # the procedure to do when data recieved
     filename = "wifi_data.csv"
     fixed_wifi = {"UoM_Wireless1": -100, "UoM_Wireless6": -100, "UoM_Wireless11": -100, "eduroam1": -100,
                   "eduroam6": -100, "eduroam11": -100, "Jungle Book10": -100,
-                  "PROLINK_H5004NK_8766E11": -100, "UNIC-wifi11": -100}  # this should be changed according to position
+                  "PROLINK_H5004NK_8766E11": -100, "UNIC-wifi11": -100}
+    # this should be changed according to position of the NodeMCU
     try:
         data = ast.literal_eval(data_recieved.decode("utf-8"))
         print(data)
@@ -34,15 +36,32 @@ def data_store(data_recieved):  # the procedure to do when data recieved
         # formatting data to a data frame and taking transpose
         data_format = pd.Series(data).to_frame().T
         print(data_format)
-        data_format.to_csv(filename, index=False, mode='a', header=(not os.path.exists(filename)))
+        data_format.to_csv(filename, index=False, mode='a',
+                           header=(not os.path.exists(filename)))
     except Exception as exc:  # exception handling
         print(exc)
 
 
+def create_csv():
+    filename = "wifi_data.csv"
+    fixed_wifi = {"id": 1, "UoM_Wireless1": -100, "UoM_Wireless6": -100, "UoM_Wireless11": -100,
+                  "eduroam1": -100, "eduroam6": -100, "eduroam11": -100, "Jungle Book10": -100,
+                  "PROLINK_H5004NK_8766E11": -100, "UNIC-wifi11": -100}
+    # this should be changed according to position of the NodeMCU
+    # creating header of the output file.
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = fixed_wifi.keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+    print("File created.")
+
+
 client = mqtt.Client()  # defining client
+create_csv()  # creating csv file header
 # assigning interrupts for client variable
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
+client.on_message = on_message
 client.connect(broker_url, broker_port)  # connect using this port
 # subscription topic - change this according to your preference
 client.subscribe("ENTC/Wifi_Outgoing", qos=1)
