1e54460c92e6ba55c5e1a6551691336354595fdf
Dinith Herath <dinithherath18332@gmail.com>
2019-08-28 23:35:07 +0530

    Added comments for understanding purposes.

--- WiFi_Data.py
+++ WiFi_Data.py
@@ -1,30 +1,31 @@
-import paho.mqtt.client as mqtt
-import ast
+import paho.mqtt.client as mqtt #mqtt client library
+import ast #library for data validation
 
-broker_url = "broker.hivemq.com"
-broker_port = 1883
+broker_url = "broker.hivemq.com" #broker URL
+broker_port = 1883 #broker port
 
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc): #on connect interrupt
     print("Connected With Result Code",(rc))
 
 
-def on_disconnect(client, userdata, rc):
+def on_disconnect(client, userdata, rc): #on disconnect interrupt
     print("Client Got Disconnected")
 
 
-def on_message(client, userdata, message):
+def on_message(client, userdata, message): #on message interrupt
     data_store(message.payload)
 
 
-def data_store(data_recieved):
+def data_store(data_recieved): #the procedure to do when data recieved
     data = ast.literal_eval(data_recieved.decode("utf-8"))
     print(data)
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
+client = mqtt.Client() #defining client
+#assigning interrupts for client variable
+client.on_connect = on_connect 
 client.on_disconnect = on_disconnect
-client.connect(broker_url, broker_port)
-client.subscribe("ENTC/Wifi_Outgoing", qos=1)
-client.loop_forever()
+client.connect(broker_url, broker_port) #connect using this port
+client.subscribe("ENTC/Wifi_Outgoing", qos=1) #subscription topic - change this according to your preference
+client.loop_forever() #loop which run forever and interrupts at any above interrupt incidences
