b6f255045d3239d44949742dc0544a209c3c5673
Dinith Herath <dinithherath18332@gmail.com>
2019-08-28 23:04:47 +0530

    Disconnect Interrupt added.

--- WiFi_Data.py
+++ WiFi_Data.py
@@ -6,8 +6,12 @@ broker_port = 1883
 def on_connect(client, userdata, flags, rc):
     print("Connected With Result Code " (rc))
 
+def on_disconnect(client, userdata, rc):
+    print("Client Got Disconnected")
+
 client = mqtt.Client()
 client.on_connect = on_connect
+client.on_disconnect = on_disconnect
 client.connect(broker_url, broker_port)
 client.subscribe("ENTC/Wifi_Outgoing", qos=1)
 client.loop_forever()
