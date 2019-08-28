1303330d2f63f18aef3e9719b866d06bc920c7e6
Dinith Herath <dinithherath18332@gmail.com>
2019-08-28 23:03:34 +0530

    On connect command added.

--- WiFi_Data.py
+++ WiFi_Data.py
@@ -3,7 +3,11 @@ import paho.mqtt.client as mqtt
 broker_url = "broker.hivemq.com"
 broker_port = 1883
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected With Result Code " (rc))
+
 client = mqtt.Client()
+client.on_connect = on_connect
 client.connect(broker_url, broker_port)
 client.subscribe("ENTC/Wifi_Outgoing", qos=1)
 client.loop_forever()
