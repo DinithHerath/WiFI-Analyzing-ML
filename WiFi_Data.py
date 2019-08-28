e4ea9ae1a324ef835edf52447a76bac7ff7871e8
Dinith Herath <dinithherath18332@gmail.com>
2019-08-28 23:24:57 +0530

    Added data store function.

--- WiFi_Data.py
+++ WiFi_Data.py
@@ -1,16 +1,26 @@
 import paho.mqtt.client as mqtt
+import ast
 
 broker_url = "broker.hivemq.com"
 broker_port = 1883
 
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected With Result Code " (rc))
+    print("Connected With Result Code"(rc))
+
 
 def on_disconnect(client, userdata, rc):
     print("Client Got Disconnected")
 
+
 def on_message(client, userdata, message):
-    print("Message Recieved: "+str(message.payload.decode()))
+    data_store(message.payload)
+
+
+def data_store(data_recieved):
+    data = ast.literal_eval(data_recieved.decode("utf-8"))
+    print(data)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
